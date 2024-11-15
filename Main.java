8829e5e67e3d3d7391e45f7fc2f6ebf51a6b4dc8
savannahlut <savannahlut@nycstudents.net>
2024-11-15 19:40:08 +0000

    Fixed test cases

--- Main.java
+++ Main.java
@@ -5,16 +5,18 @@ public class Main
         boolean[][] schedule = new boolean[8][60];
         for(int i = 10; i < 15; i++) schedule[1][i] = true;
         for(int i = 30; i < 45; i++) schedule[1][i] = true;
+        AppointmentBook what = new AppointmentBook(schedule);
+        what.printPeriod(2);
+        System.out.println(what.findFreeBlock(2, 15));
         
+        schedule = new boolean[8][60];
         for(int i = 25; i < 30; i++) schedule[1][i] = true;
         for(int i = 0; i < 15; i++) schedule[2][i] = true;
         for(int i = 41; i < 60; i++) schedule[2][i] = true;
         for(int i = 5; i < 30; i++) schedule[3][i] = true;
         for(int i = 44; i < 60; i++) schedule[3][i] = true;
         AppointmentBook wumbo = new AppointmentBook(schedule);
-        wumbo.printPeriod(2);
-        System.out.println(wumbo.findFreeBlock(2, 15));
-        
+
         int period = 2;
         while(period < 5)
         {
